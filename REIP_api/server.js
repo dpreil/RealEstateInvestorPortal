3c88a7d03881d55db2a03f1f9f5cbaadd374ca0c
DP <dzpreil@gmail.com>
2019-06-25 17:50:26 +0300

    linked login from front to back and rerendered to Hello World page based on pug view

--- REIP_api/server.js
+++ REIP_api/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const pug = require('pug');
+const bodyParser = require('body-parser');
 
+const user = {
+	"username":"test",
+	"password":"pickle"
+}
 
 const app = express();
+app.use(bodyParser.json({extended:false}));
 app.set('view engine', 'pug')
 
 console.log(__dirname);
@@ -14,3 +20,14 @@ app.use(express.static(filePath));
 app.get('/',(req,res) => res.sendFile("index.html"))
 
 app.listen(3000,()=>console.log("listening on port 3000"))
+
+app.post('/login',(req,res) => {
+	if((req.body.username === user.username) && (req.body.password === user.password)){
+		console.log("login successful");
+		res.status(200).render("portfolio_page");
+	} else {
+		console.log("login failed")
+		res.status(400).send("please reenter your credentials.")
+	}
+
+})
